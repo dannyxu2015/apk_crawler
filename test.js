b842a7f38a668154e12d3e017213b1c9a15018b2
dannyxu <dannyxu@gmail.com>
2016-04-29 07:49:57 +0000

    Update test.js

--- test.js
+++ test.js
@@ -5,16 +5,14 @@ var casper = require('casper').create({
   }
 });
 
-//casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4');
-
-var fs = require('fs');
 var downloadUrl = '';
-var originApks = fs.list('.');
-
-//casper.on('remote.message', function(message) {
-//    this.echo(message);
-//});
-
+/*
+ * output download url && apk file name, which delimited by '|'
+ *
+ * Todo: use command line arguments to input detail url of the android app,
+ *       refer to: http://docs.casperjs.org/en/latest/cli.html
+ *                 http://stackoverflow.com/questions/21765178/how-to-pass-a-variable-to-a-casperjs-script-through-the-command-line
+*/
 casper.start('http://coolapk.com/apk/com.obviousengine.seene.android.core').thenClick('a.btn.btn-success.ex-btn-glyphicon', function() {
     //this.echo('download button clicked');
 });
@@ -31,64 +29,5 @@ casper.then(function dumpHeaders(){
   });
   //console.log('-------------------------');
 });
-
-/*
-casper.then(function() {
-    if (downloadUrl != '') {
-      //console.log('downloading from ' + downloadUrl + ' ...');
-      fn = downloadUrl.match(/([a-zA-Z0-9\s_\\.\-:]+\.apk)/)[1];
-      console.log('downloading ' + fn + '...');
-      casper.download(downloadUrl, fn);
-    }
-});
-*/
-
-/*
-casper.then(function() {
-    console.log('downloading by open download url ...');
-    casper.open(downloadUrl);
-});
-*/
-
-/*
-casper.on('resource.received', function (resource) {
-     "use strict";
-     if ( (resource.url.indexOf(".apk") !== -1)) {
-        this.echo('received from: ' + resource.url);
-        var url, file;
-        url = resource.url;
-        file = "yyy.apk";
-        try {
-            this.echo("Attempting to download file " + file);
-            var fs = require('fs');
-            casper.download(resource.url, fs.workingDirectory+'/'+file);
-        } catch (e) {
-            this.echo('ERROR --> ' + e);
-                    }
-     }
- });
-*/
-
-/*
-casper.start('http://coolapk.com/apk/com.obviousengine.seene.android.core', function() {
-    firstUrl = this.getCurrentUrl();
-    this.echo('Page title is: ' + this.evaluate(function() {
-        console.log('from evaluate: ' + document.title);
-        return document.title;
-    }), 'INFO'); // Will be printed in green on the console
-    this.evaluate(function(s) {
-        document.querySelector(s).click();
-    }, "a.btn.btn-success.ex-btn-glyphicon");
-});
-*/
-
-/*
-casper.waitFor(function check() {
-    console.log((fs.list('.').toString() === originApks.toString()).toString());
-    return fs.list('.').toString() != originApks.toString();
-}, function then() {
-    console.log('u got it, files: ' + fs.list('.').toString());
-});
-*/
 
 casper.run();
