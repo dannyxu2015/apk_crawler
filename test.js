4632eea20d93a36ab2a59ba2d5f7dc5f968598e2
dannyxu <dannyxu@gmail.com>
2016-04-29 10:04:57 +0000

    use CLI

--- test.js
+++ test.js
@@ -1,33 +1,35 @@
+/*
+ * Usage:
+ *   casperjs test.js
+ *
+ * @output String
+ *   download url && apk file name, delimited by '|'
+ *
+ */
+
 var casper = require('casper').create({
-    waitTimeout: 50000,
+    waitTimeout: 5000,
     pageSettings: {
         webSecurityEnabled: false  //no CORS
   }
 });
 
+var url = 'http://coolapk.com/apk/com.obviousengine.seene.android.core';
 var downloadUrl = '';
-/*
- * output download url && apk file name, which delimited by '|'
- *
- * Todo: use command line arguments to input detail url of the android app,
- *       refer to: http://docs.casperjs.org/en/latest/cli.html
- *                 http://stackoverflow.com/questions/21765178/how-to-pass-a-variable-to-a-casperjs-script-through-the-command-line
-*/
-casper.start('http://coolapk.com/apk/com.obviousengine.seene.android.core').thenClick('a.btn.btn-success.ex-btn-glyphicon', function() {
-    //this.echo('download button clicked');
-});
+
+//refer: http://docs.casperjs.org/en/latest/cli.html
+if (casper.cli.has(0)) {
+  url = casper.cli.get(0);
+}
+casper.start(url).thenClick('a.btn.btn-success.ex-btn-glyphicon');
 
 casper.then(function dumpHeaders(){
-  //console.log('Response headers');
-  //console.log('-------------------------');
   this.currentResponse.headers.forEach(function(header){
-    //console.log(header.name +': '+ header.value);
     if (header.name === 'Location') {
         downloadUrl = header.value;
         console.log(downloadUrl + '|' + downloadUrl.match(/([a-zA-Z0-9\s_\\.\-]+\.apk)/)[1]);
     }
   });
-  //console.log('-------------------------');
 });
 
 casper.run();
